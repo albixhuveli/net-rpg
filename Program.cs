290996a463ccf34fe541e1cfe4310da9e17d3187
albixhuveli <albi.xhuveli.601@gmail.com>
2024-06-03 20:06:44 -0400

    fixed automapper

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>(); 
 //webapi know to use charserv class whenever controller wants to inject icharserv 
 
