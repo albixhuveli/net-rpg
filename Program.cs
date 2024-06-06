7a0fe5056c10f2b12b8a0db7e4b48a3bf2ed7197
albixhuveli <albi.xhuveli.601@gmail.com>
2024-06-05 22:58:54 -0400

    added DBcontext and connection string

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 global using net_RPG.Services.CharacterService;
 global using net_RPG.Dtos.Character;
 global using net_RPG.Models;
+global using Microsoft.EntityFrameworkCore;
+global using net_RPG.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
